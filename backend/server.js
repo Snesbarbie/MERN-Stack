d378146428996b8db447fcb110284dcee2e569e2
Snesbarbie <65239172+Snesbarbie@users.noreply.github.com>
2022-11-02 14:31:29 +0000

    rename test

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,14 @@ const express = require('express')
 require('dotenv').config()
 
 const app = express()
+const workoutRoutes = require('./routes/workouts')
 
 app.use((req, res, next)=>{
     console.log(req.path, req.method)
     next()
 })
 
-app.get('/', (req, res) => {
-    res.json({msg: 'Welcome'})
-})
+app.use('/api/workouts',workoutRoutes)
 
-app.listen(process.env.PORT, () =>{console.log("Listening to " + process.env.PORT)})
+app.listen(process.env.PORT, () =>{
+    console.log("Listening to " + process.env.PORT)})
