e555268bed3f1225640e03785a543add71343786
Snesbarbie <65239172+Snesbarbie@users.noreply.github.com>
2022-11-02 13:53:49 +0000

    END OF VID 2

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 require('dotenv').config()
 
-
 const app = express()
+
+app.use((req, res, next)=>{
+    console.log(req.path, req.method)
+    next()
+})
 
 app.get('/', (req, res) => {
     res.json({msg: 'Welcome'})
